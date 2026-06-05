8583aff375484096c86532fa3cfb4cf7a3fab791
LeoStefL1 <stefanidi.leonardo@gmail.com>
2026-06-06 01:54:55 +0200

    initial data analysis report in proposal.qmd, summaries of variables and pictures of graphs for the proposal in Skript

--- Skript.R
+++ Skript.R
@@ -2,11 +2,12 @@ library("skimr")
 library("palmerpenguins")
 library("naniar")
 library("ggplot2")
+library("dplyr")
 
 View(penguins_raw)
 head(penguins_raw)
 data <- penguins_raw
-
+glimpse(data)
 
 skim(penguins_raw)
 #not every essential variable is complete, but the completion rates are relatively high
@@ -14,7 +15,7 @@ skim(penguins_raw)
 miss_var_summary(data)
 vis_miss(data)
 
-bill_hist <- ggplot(data, aes(x = `Culmen Depth (mm)`)) +
+ggplot(data, aes(x = `Culmen Depth (mm)`)) +
   geom_histogram(bins = 30) +
   labs(title = "Distribution of Bill Depth",
        x = "Bill Depth in mm", y = "Count")
@@ -22,4 +23,8 @@ bill_hist <- ggplot(data, aes(x = `Culmen Depth (mm)`)) +
 ggplot(data, aes(x = `Culmen Depth (mm)`, y = `Body Mass (g)`, colour = `Species`)) +
   geom_point(alpha = 0.6) +
   labs(title = "Relationship between bill depth and weight")
+
+summary(data$`Body Mass (g)`)
+summary(data$`Flipper Length (mm)`)
+summary(data$`Culmen Depth (mm)`)
 
